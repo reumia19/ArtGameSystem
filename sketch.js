e1d4949f3d54a5ba1bf6048653c606b14bbf9a6f
reumia19 <56555888+reumia19@users.noreply.github.com>
2019-11-12 23:33:57 +0900

    제발 충돌해주세요 ㅜㅜ

--- sketch.js
+++ sketch.js
@@ -1,15 +1,22 @@
 let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
-
+let paddle,wallTop,wallBottom;
+var s;
 
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
 
- paddleA = createSprite(30, height/2, 10, 100);
- paddleA.immovable = true;
+ paddle = createSprite(0, height/2, 10, height);
+ paddle.immovable = true;
+
+  wallTop = createSprite(width/2, -30/2, width, 30);
+  wallTop.immovable = true;
+
+  wallBottom = createSprite(width/2, height+30/2, width, 30);
+  wallBottom.immovable = true;
 
 }
 
@@ -18,20 +25,22 @@ function draw() {
    fill(0);
   textAlign(CENTER);
   text('Click to create a new sprite', width/2, height/2);
-
-    drawSprites();
-
+ 
+  drawSprites();
 }
 
 function mousePressed() {
-
   //create a sprite at the mouse position and store it in a temporary variable
-  var s = createSprite(mouseX, mouseY, 30, 30);
+  s = createSprite(mouseX, mouseY, 30, 30);
+
   //if no image or animation is associated it will be a rectancle of the specified size
   //and a random color
-
   //now you can use the variable to set properties
   //e.g. a random velocity on the x and y coordinates
+  s.bounce(paddle);
   s.velocity.x = random(-5, 5);
   s.velocity.y = random(-5, 5);
+  ;
+    
 }
+
