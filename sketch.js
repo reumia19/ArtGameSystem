1bbc79ba07a91eea014bd456f22ed4af51aec530
reumia19 <56555888+reumia19@users.noreply.github.com>
2019-11-15 01:54:09 +0900

    hi, this!

--- sketch.js
+++ sketch.js
@@ -24,11 +24,13 @@ function draw() {
 function mousePressed() {
 
   //create a sprite at the mouse position and store it in a temporary variable
+
+  this.s = createSprite(mouseX, mouseY, 30, 30);
   //if no image or animation is associated it will be a rectancle of the specified size
   //and a random color
 
   //now you can use the variable to set properties
   //e.g. a random velocity on the x and y coordinates
-  s.velocity.x = random(-5, 5);
-  s.velocity.y = random(-5, 5);
+  this.s.velocity.x = random(-5, 5);
+  this.s.velocity.y = random(-5, 5);
 }
