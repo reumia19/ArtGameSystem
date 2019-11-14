c0770ab9fcb0ec0bca0cbaa9a5105682898b5878
reumia19 <56555888+reumia19@users.noreply.github.com>
2019-11-15 01:43:28 +0900

    make group

--- sketch.js
+++ sketch.js
@@ -24,8 +24,8 @@ function draw() {
 function mousePressed() {
 
   //create a sprite at the mouse position and store it in a temporary variable
-
-  var s = createSprite(mouseX, mouseY, 30, 30);
+   var s = new Groups();
+   s = createSprite(mouseX, mouseY, 30, 30);
   //if no image or animation is associated it will be a rectancle of the specified size
   //and a random color
 
