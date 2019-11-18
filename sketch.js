5c4423b9d1cf5502aa9c885b5fa4a18f3db8df5b
reumia19 <56555888+reumia19@users.noreply.github.com>
2019-11-18 23:07:04 +0900

    position update

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function setup() {
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
 
 var img = loadImage("image/picxenk.png");
- imageSprite = createSprite(mouseX, mouseY,50,50);
+ imageSprite = createSprite(width/2, heignt/2,50,50);
  imageSprite.addImage(img);
 
 }
