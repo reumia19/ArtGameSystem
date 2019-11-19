9c8d161bcafba3d0aacfd6b7a442e876c1fba88a
reumia19 <56555888+reumia19@users.noreply.github.com>
2019-11-19 16:34:23 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,12 +9,15 @@ let sound_hit;
 
 
 function preload() {
-  sound_hit = loadSound('hit.wav');
+  // sound_hit = loadSound('hit.wav');
 }
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
+
+  
+  resetGame();
 
 }
 
@@ -30,7 +33,7 @@ function draw() {
 
   
   if (frog.bounce(car1)) {
-    sound_hit.play();
+    // sound_hit.play();
   }
   
   // 충돌 시 사운드 효과에 대한 또 다른 방법
@@ -47,6 +50,13 @@ function draw() {
  
 }
 
+function resetGame() {
+  frog = createSprite(width/2, height-30, 20, 40);
+  goal = createSprite(width/2, 0, width, 4);
+  car1 = createSprite(0, height/2, 60, 30);
+  
+  car1.setVelocity(random(3, 10), 0);
+}
 
 function keyPressed() {
   if (keyCode == UP_ARROW) {
