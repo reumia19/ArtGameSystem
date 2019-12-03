15d1d67087249e00b979f8d0447936f5ffe5716b
reumia19 <56555888+reumia19@users.noreply.github.com>
2019-12-03 11:59:02 +0900

    feat : rect

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@ let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
 
+let xCor = ;
+let yCor = ;
+
 function preload() {
   // sound_hit = loadSound('hit.wav');
 }
@@ -10,12 +13,11 @@ function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
 
-
 }
 
 function draw() {
    background(50);
+   rect(xCor,yCor,xCor+10,yCor+10);
 
- 
 }
 
